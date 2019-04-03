5730a86b35895fe988da0f7e630cef5be74806ee
supradeepbhat <sbharadwaj@hawk.iit.edu>
2019-04-03 09:53:14 -0700

    Created Box Component. Closes #2

---
+++ src/components/Box.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(<div></div>);
+    }
+}
