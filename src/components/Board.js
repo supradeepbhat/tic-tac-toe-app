98415d9769f3335dc464e7f835014e2194e024ac
supradeepbhat <sbharadwaj@hawk.iit.edu>
2019-04-04 18:05:51 -0700

    Created the function to calculate the winner. Closes #9

--- src/components/Board.js
+++ src/components/Board.js
@@ -19,6 +19,27 @@ class Board extends Component {
         return ( <Box onClick={()=>{this.handleBoxClick(i)}} value={this.state.boxValues[i]}/>);
     }
 
+    calculateWinner(boxes){
+        let moves = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for(let i=0; i< moves.length; i++){
+            let [b1, b2, b3] = moves[i];
+            if(boxes[b1] && boxes[b1] === boxes[b2] && boxes[b1] === boxes[b3]){
+                return boxes[b1];
+            }
+        }
+
+        return null;
+    }
+
 
     render(){
         return(<div>
